021ae0b1966d501c69fedba69bd2b4af5d192f4a
dengguojian <dengguojian>
2017-11-07 14:00:11 +0800

    calculate the similarity of cosine

--- lesson8/evaluate_model.py
+++ lesson8/evaluate_model.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
-import d_vector as d_v
+import dnn_d_vector as d_v
 import data_process as dp
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import stats
 
 test_data, _ = dp.get_test_data()
 
@@ -23,8 +25,26 @@ with tf.Session() as session:
     representation_data = [data[0] for data in test_data]
     # print(len(representation_data))
     representation_vector = []
+    print(len(representation_data))
     for data in representation_data:
         # print(data)
-        vector = session.run([ds], feed_dict={X: data})
-        # print(len(vector))
-        print(vector)
+        [vector] = session.run([ds], feed_dict={X: data, d_v.train_phase: False})
+        representation_vector.append(np.mean(vector, axis=0))
+    # the representaion speaker matrix shape is (speaker_num, 256). 
+    # 256 is dim of d-vector
+    representation_speaker_matrix = np.array(representation_vector)
+
+    correct_num = 0
+    test_count = 0
+    for index, speaker_data in enumerate(test_data):
+        for test_utterance in speaker_data[1:]:
+            [test_vector] = session.run([ds], feed_dict={X: test_utterance, d_v.train_phase: False})
+            # the shape of cos_sim is (speaker_num, test_frame_num)
+            cos_sim = cosine_similarity(representation_vector, test_vector)
+
+            predicts = np.argmax(cos_sim, axis=0)
+            mode, count = stats.mode(predicts)
+            correct_num += (mode[0] == index and count[0] > 20)
+            test_count += 1
+
+    print(correct_num / test_count)

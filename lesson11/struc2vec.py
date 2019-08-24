779f5c14c4eb421776c2477199fc7e440dfd9288
guojian.deng <guojian.deng@vipshop.com>
2019-08-24 16:21:03 +0800

    增加部分代码

--- lesson11/struc2vec.py
+++ lesson11/struc2vec.py
@@ -97,19 +97,56 @@ class Struc2Vec():
                 dist_func = cost
 
             if os.path.exists(self.temp_path + 'degreelist.pkl'):
-                degreeList = pd.read_pickle(self.temp_path + 'degreelist.pkl')
+                degree_list = pd.read_pickle(self.temp_path + 'degreelist.pkl')
             else:
-                degreeList = self._compute_ordered_degreelist(max_num_layers)
-                pd.to_pickle(degreeList, self.temp_path + 'degreelist.pkl')
+                degree_list = self._compute_ordered_degreelist(max_num_layers)
+                pd.to_pickle(degree_list, self.temp_path + 'degreelist.pkl')
             
             if self.opt2_reduce_sim_calc:
-                degree = self._create_vectors()
+                degrees = self._create_vectors()
+                degree_list_selected = {}
+                vertices = {}
+                n_nodes = len(self.graph.nodes())
+                for v in self.graph.nodes():
+                    # 获取相邻的节点（度数越相近越相邻）
+                    nbs = get_vertices(v, len(self.graph[v]), degrees, n_nodes)
+                    vertices[v] = nbs
+                    degree_list_selected[v] = degree_list[v]
+                    for n in nbs:
+                        degree_list_selected[n] = degree_list[n]
+            else:
+                vertices = {}
+                for v in degree_list:
+                    vertices[v] = [vd for vd in degree_list.keys() if vd > v]
+
+            results = Parallel(n_jobs=workers, verbose=verbose,)(delayed(compute_dtw_dist)(part_list, degree_list, dist_func) for part_list in partition_dict(vertices, workers))
+            dtw_dist = dict((ChainMap(*results)))
+            structural_dist = convert_dtw_struc_dist(dtw_dist)
+            pd.to_pickle(structural_dist, self.temp_path + 'structural_dist.pkl')
+        return structural_dist
 
     def _create_vectors(self):
-        degrees = {}
-        degrees_sorted = set()
-        G = self.graph
-        # for v in self.
+        degrees = {} # key: graph中的度数，value: 度数的节点
+        degrees_sorted = set() # 存放graph当中的度数
+        for v in self.graph.nodes():
+            degree = len(self.graph[v])
+            degrees_sorted.add(degree)
+            if (degree not in degrees):
+                degrees[degree] = {}
+                degrees[degree]['vertices'] = []
+            degrees[degree]['vertices'].append(v)
+        degrees_sorted = np.array(list(degrees_sorted), dtype='int')
+        degrees_sorted = np.sort(degrees_sorted)
+
+        l = len(degrees_sorted)
+        for index, degree in enumerate(degrees_sorted):
+            if (index > 0):
+                # 记录比该度数小的前一个度数值
+                degrees[degree]['before'] = degrees_sorted[index - 1]
+            if (index < (l - 1)):
+                # 记录比该度数大的后一个度数值
+                degrees[degree]['after'] = degrees_sorted[index + 1]
+        return degrees
 
 def cost(a, b):
     ep = 0.5
@@ -129,5 +166,83 @@ def cost_max(a, b):
     mi = min(a[0], b[0]) + ep
     return ((m / mi) - 1) * max(a[1], b[1])
 
+def get_vertices(v, degree_v, degrees, n_nodes):
+    # 通过传入的度数，找出对应的节点
+    a_vertices_selected = 2 * math.log(n_nodes, 2) # 限制选择节点的个数
+    vertices = []
+    try:
+        c_v = 0
+        for v2 in degrees[degree_v]['vertices']:
+            if (v != v2):
+                vertices.append(v2)
+                c_v += 1
+                if (c_v > a_vertices_selected):
+                    raise StopIteration
 
+        # 判断是否为最小的度
+        if ('before' not in degrees[degree_v]):
+            degree_b = -1
+        else:
+            degree_b = degrees[degree_v]['before']
+        
+        # 判断是否为最大的度
+        if ('after' not in degrees[degree_v]):
+            degree_a = -1
+        else:
+            degree_a = degrees[degree_v]['after']
 
+        # graph中只有一个度
+        if (degree_b == -1 and degree_a == -1):
+            raise StopIteration
+        degree_now = verify_degrees(degrees, degree_v, degree_a, degree_b)
+
+        while True:
+            for v2 in degrees[degree_now]['vertices']:
+                if (v != v2):
+                    vertices.append(v2)
+                    c_v += 1
+                    if (c_v > a_vertices_selected):
+                        raise StopIteration
+
+            if (degree_now == degree_b):
+                if ('before' not in degrees[degree_b]):
+                    degree_b = -1
+                else:
+                    degree_b = degrees[degree_b]['before']
+            else:
+                if ('after' not in degrees[degree_a]):
+                    degree_a = -1
+                else:
+                    degree_a = degrees[degree_a]['after']
+            # TODO：是否需要删除这个判断，因为之前已经判断图中只有一个度
+            if (degree_b == -1 and degree_a == -1):
+                raise StopIteration
+            degree_now = verify_degrees(degrees, degree_v, degree_a, degree_b)
+    except StopIteration:
+        return list(vertices)
+    return list(vertices)
+
+def verify_degrees(degrees, degree_v_root, degree_a, degree_b):
+    if (degree_b == -1):
+        degree_now = degree_a
+    elif (degree_a == -1):
+        degree_now = degree_b
+    # 选择距离较近的root的那个度
+    elif (abs(degree_b, degree_v_root) < abs(degree_a - degree_v_root)):
+        degree_now = degree_b
+    else:
+        degree_now = degree_a
+    return degree_now
+
+def compute_dtw_dist(part_list, degree_list, dist_func):
+    dtw_dict = {}
+    for v1, nbs in part_list:
+        lists_v1 = degree_list[v1] # orderd degree list of v1
+        for v2 in nbs:
+            lists_v2 = degree_list[v2] # orderd degree list of v2
+            max_layer = min(len(lists_v1), len(lists_v2))
+            dtw_dist[v1, v2] = {}
+            for layer in range(0, max_layer):
+                dist, path = fastdtw(lists_v1[layer], lists_v2[layer], radius=1, dist=dist_func)
+                dtw_dict[v1, v2][layer] = dist
+    return dtw_dict
